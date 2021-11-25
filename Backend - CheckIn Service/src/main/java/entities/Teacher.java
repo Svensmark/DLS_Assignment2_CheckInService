9e5a6824172ce49c8ebfeaabd95c7e4325c153e1
Emil <emil.svensmark@gmail.com>
2021-11-25 13:30:52 +0100

    First commit

---
+++ Backend - CheckIn Service/src/main/java/entities/Teacher.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Emil Svensmark
+ */
+@Entity
+public class Teacher implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    private int id;
+    private String name;
+    
+    public Teacher() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
